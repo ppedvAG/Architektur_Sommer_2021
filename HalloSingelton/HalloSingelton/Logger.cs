b538c66e8a2a28d479a0fee77ba2d13955892617
André <ruland@outlook.de>
2021-06-28 14:31:36 +0200

    Singelton ist einzigartig

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    public class Logger
+    {
+
+        private static Logger instance = null;
+        private static object _syncObj = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_syncObj)
+                {
+                    if (instance == null)
+                        instance = new Logger();
+                }
+                return instance;
+            }
+        }
+
+        static int instanceCount = 0;
+        private Logger()
+        {
+            Log($"Logger #{++instanceCount} startet");
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 20; i++)
+            {
+                //var logger = new Logger();
+                Task.Run(() => Logger.Instance.Log("Test #1"));
+                Task.Run(() => Logger.Instance.Log("Test #2"));
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
