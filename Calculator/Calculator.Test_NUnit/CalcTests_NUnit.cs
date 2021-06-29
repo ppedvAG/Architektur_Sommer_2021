8db72a5a6067bd65d5ff3eb16c05196063db234b
André <AndreR@ppedv.de>
2021-06-29 11:25:10 +0200

    NUnit und xUnit sind nun auch dabei

---
+++ Calculator/Calculator.Test_NUnit/CalcTests_NUnit.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Calculator.Test_NUnit
+{
+    [TestFixture]
+    public class CalcTests_NUnit
+    {
+        [Test]
+        public void Sum_1_and_6_results_7()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(1, 6);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-1, -4, -5)]
+        [TestCase(-4, 10, 6)]
+        public void Sum(int a, int b, int expected)
+        {
+            Calc calc = new Calc();
+
+            int result = calc.Sum(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Calculator/Calculator.Test_xUnit/CalcTest_xUnit.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+
+namespace Calculator.Test_xUnit
+{
+    public class CalcTest_xUnit
+    {
+        [Fact]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-1, -4, -5)]
+        [InlineData(-4, 10, 6)]
+        public void Sum(int a, int b, int expected)
+        {
+            Calc calc = new Calc();
+
+            int result = calc.Sum(a, b);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
