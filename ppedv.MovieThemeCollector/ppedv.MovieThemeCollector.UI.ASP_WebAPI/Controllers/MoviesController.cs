94adc13689490682f3a56e06de7ab10c9f406148
André <AndreR@ppedv.de>
2021-07-05 16:49:49 +0200

    ASP Core WebAPI mit DTOs und AutoMapper

---
+++ ppedv.MovieThemeCollector/ppedv.MovieThemeCollector.UI.ASP_WebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.MovieThemeCollector.Contracts;
+using ppedv.MovieThemeCollector.Logic;
+using ppedv.MovieThemeCollector.UI.ASP_WebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ppedv.MovieThemeCollector.UI.ASP_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        Core core = new Core(null, new Data.EFCore.EfUnitOfWork());
+        MapperConfiguration mapperConfiguration;
+
+        public MoviesController()
+        {
+
+            mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Movie, MovieDTO>()
+                   .ForMember(m => m.PubDate, dto => dto.MapFrom(x => x.Published))
+                   .ReverseMap();
+                cfg.CreateMap<Person, PersonDTO>().ReverseMap();
+            });
+        }
+
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<MovieDTO> Get()
+        {
+            var movies = core.UnitOfWork.MovieRepository.Query().ToList();
+            var mapper = mapperConfiguration.CreateMapper();
+
+            foreach (var m in movies)
+            {
+                yield return mapper.Map<MovieDTO>(m);
+            }
+        }
+
+
+        private IEnumerable<MovieDTO> GetMitYield_OhneAutoMapper()
+        {
+            var movies = core.UnitOfWork.MovieRepository.Query().ToList();
+
+            foreach (var m in movies)
+            {
+                yield return new MovieDTO()
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Length = m.Length,
+                    PubDate = m.Published
+                };
+            }
+        }
+
+        private IEnumerable<MovieDTO> GetOhneYield()
+        {
+            var movies = core.UnitOfWork.MovieRepository.Query().ToList();
+
+            List<MovieDTO> movieDTOs = new List<MovieDTO>();
+            foreach (var m in movies)
+            {
+                movieDTOs.Add(new MovieDTO());
+            }
+            return movieDTOs;
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public MovieDTO Get(int id)
+        {
+            return mapperConfiguration.CreateMapper().Map<MovieDTO>(core.UnitOfWork.MovieRepository.GetById(id));
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] MovieDTO value)
+        {
+            var movie = mapperConfiguration.CreateMapper().Map<Movie>(value);
+            core.UnitOfWork.MovieRepository.Add(movie);
+            core.UnitOfWork.Save();
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] MovieDTO value)
+        {
+            var movie = mapperConfiguration.CreateMapper().Map<Movie>(value);
+            core.UnitOfWork.MovieRepository.Update(movie);
+            core.UnitOfWork.Save();
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            core.UnitOfWork.MovieRepository.DeleteById(id);
+            core.UnitOfWork.Save();
+        }
+    }
+}
---
+++ ppedv.MovieThemeCollector/ppedv.MovieThemeCollector.UI.ASP_WebAPI/Model/MovieDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ppedv.MovieThemeCollector.UI.ASP_WebAPI.Model
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public DateTime PubDate { get; set; }
+        public string Title { get; set; }
+        public int Length { get; set; }
+
+        public PersonDTO[] Actors { get; set; }
+    }
+
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
