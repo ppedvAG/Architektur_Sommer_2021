f83d6ef07b31b4c6a6e515d81fd438f1e76af5f8
André <AndreR@ppedv.de>
2021-07-05 12:16:21 +0200

    Dependency Injection Manual für das Device in der Console

--- ppedv.MovieThemeCollector/ppedv.MovieThemeCollector.UI.DevConsole/Program.cs
+++ ppedv.MovieThemeCollector/ppedv.MovieThemeCollector.UI.DevConsole/Program.cs
@@ -1,8 +1,10 @@
 ﻿using ppedv.MovieThemeCollector.Common;
 using ppedv.MovieThemeCollector.Contracts;
+using ppedv.MovieThemeCollector.Contracts.Interfaces;
 using ppedv.MovieThemeCollector.Logic;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace ppedv.MovieThemeCollector.UI.DevConsole
 {
@@ -13,7 +15,22 @@ namespace ppedv.MovieThemeCollector.UI.DevConsole
             Logger.Instance.Info("Consolen UI gestartet");
             Console.WriteLine("Hello World!");
 
-            var core = new Core(new Device.ACME.ACMESoundplayer2000(), new Data.EFCore.EfUnitOfWork());
+#if DEBUG
+            var deviceLibPath = @$"C:\Users\Fred\source\repos\ppedvAG\Architektur_Sommer_2021\ppedv.MovieThemeCollector\ppedv.MovieThemeCollector.Device.ACME\bin\Debug\net5.0\ppedv.MovieThemeCollector.Device.ACME.dll";
+#else
+            var deviceLibPath= @$"C:\Users\Fred\source\repos\ppedvAG\Architektur_Sommer_2021\ppedv.MovieThemeCollector\ppedv.MovieThemeCollector.Device.ACME\bin\Release\net5.0\ppedv.MovieThemeCollector.Device.ACME.dll";
+#endif
+
+            Console.WriteLine(deviceLibPath);
+
+            var ass = Assembly.LoadFrom(deviceLibPath);
+            Type typeWithIDevice = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IDevice)));
+            IDevice device = (IDevice)Activator.CreateInstance(typeWithIDevice);
+
+            var core = new Core(device, new Data.EFCore.EfUnitOfWork());
+            //var core = new Core(new Device.ACME.ACMESoundplayer2000(), new Data.EFCore.EfUnitOfWork());
+
+
             core.Device.Play(300, 200);
 
             var query = core.UnitOfWork.GetRepo<Movie>().Query().Where(x => !string.IsNullOrWhiteSpace(x.Title));
@@ -33,9 +50,17 @@ namespace ppedv.MovieThemeCollector.UI.DevConsole
             //CREATE PROCEDURE dbo.GetMoviesOrderdByYear
             //AS
             //SELECT* FROM Movies ORDER BY DATEPART(Year, Published)
-            foreach (var item in core.UnitOfWork.MovieRepository.GetMovieFromThatSpecialStroedProc())
+            try
             {
-                Console.WriteLine($"STORED-PROC: {item.Title}");
+                foreach (var item in core.UnitOfWork.MovieRepository.GetMovieFromThatSpecialStroedProc())
+                {
+                    Console.WriteLine($"STORED-PROC: {item.Title}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Stored Proc konnte nicht aufgerufen werden: {ex.Message}");
             }
 
             Console.WriteLine("Ende");
