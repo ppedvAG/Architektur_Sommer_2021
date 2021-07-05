090fc16753769689dec1e93166f35536fda6d64e
André <AndreR@ppedv.de>
2021-07-05 19:09:19 +0200

    Swagger client

---
+++ ppedv.MovieThemeCollector/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            swaggerClient sc = new swaggerClient("https://localhost:5001/", new System.Net.Http.HttpClient());
+            
+            sc.MoviesAllAsync().Result.ToList().ForEach(x => Console.WriteLine(x.Title));
+
+        }
+    }
+}
