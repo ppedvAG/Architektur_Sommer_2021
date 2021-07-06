ee5298c93a0d32b56bac101ba9b0e8d0fe2e9ab2
André <AndreR@ppedv.de>
2021-07-06 09:21:16 +0200

    OpenAPI Swagger Client

---
+++ ppedv.MovieThemeCollector/ppedv.MovieThemeCollector.UI.ASP_WebAPI.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ppedv.MovieThemeCollector.UI.ASP_WebAPI.Client
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            swaggerClient sc = new swaggerClient("https://localhost:5001", new System.Net.Http.HttpClient());
+            foreach (var item in sc.MoviesAllAsync().Result.ToList())
+            {
+                Console.WriteLine(item.Title);
+            }
+
+            Console.ReadLine();
+        }
+            
+    }
+}
